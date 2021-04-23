ae02c927d45c11681d340036a9b46dfe887fedef
Joseph Giunta <jgnohack@gmail.com>
2021-04-23 11:47:50 -0700

    move app to components, add Keg component

---
+++ tap-room/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App(){
+  return (
+    <React.Fragment>
+      <h1>Tap House</h1>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
